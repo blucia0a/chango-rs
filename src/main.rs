dc7479f57e0d6ec11b4d969cd5077561aa8834ef
Brandon Lucia <blucia@gmail.com>
2018-05-22 16:40:11 -0400

    documentation and clean up

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,12 @@ const NUMGENS: usize = 8;
 
 fn main() {
 
+  //Initialize an unsafe array of atomically reference counted pointers to
+  //float32 amplitude values.
   let mut amps: [Arc<Mutex<f32>>; NUMGENS]; 
-  println!("did the unsafe thing");
-
   unsafe{
-  amps = mem::uninitialized();
+
+    amps = mem::uninitialized();
     for elem in &mut amps[..] {
   
       ptr::write( elem, Arc::new( Mutex::new( 0.5 ) ) );
@@ -24,42 +25,39 @@ fn main() {
     }
   }
   
-  println!("did the init");
-  
-  { 
+  {
+    //Clone amps into the do_audio thread so amplitude values are accessible
     let ampsa = amps.clone();
     let _ = thread::spawn(move || {
         do_audio(ampsa);
     }); 
   }
 
-  //Apparently OpenCV doesn't like to be in a thread...
+  //Apparently OpenCV doesn't like to be in a thread, so video goes here.
+  //Also, need to clone amps here so the video thread can update the amplitude
+  //array
   {
     let ampsv = amps.clone();
     let video = move ||{
+
       let cap = VideoCapture::new(0);
       assert!(cap.is_open());
       
-      let mut framecount = 2; //3 times per second
       highgui_named_window("Window", WindowFlag::Autosize).unwrap();
       while let Some(image) = cap.read() {
    
-        framecount = framecount - 1; 
-        if framecount == 0 {
-          framecount = 2;
-          for i in 0..NUMGENS{
-            {
-              let mut a = ampsv[i].lock().unwrap();
-              *a = *a + 0.01;
-              if *a >= 1.0 { *a = 0.0; }
-            }
-          }  
-        }
+        for i in 0..NUMGENS{
+          let mut a = ampsv[i].lock().unwrap();
+          *a = *a + 0.01;
+          if *a >= 1.0 { *a = 0.0; }
+        }  
 
         let windowname = "Rusty Chango"; 
         image.show(&windowname, 1).unwrap();
       }
     };
+
+    //Invoke the closure above.  This seems weird...
     video();
   }
   
@@ -78,25 +76,37 @@ fn do_audio(vamps: [Arc<Mutex<f32>>; NUMGENS]) {
 
   let c_scale = [261.,293.,329.,349.,392.,440.,493.,523.]; 
 
-  //Initialize an array of wave generator closures
+  //Declares an array of wave generator closures
   let mut gens: [_; NUMGENS] = unsafe {std::mem::uninitialized()};
+
+  //Declares a local amplitude scale factor array.  This array gets updated
+  //periodically using the values from vamps, the shared one 
   let mut amps: [_; NUMGENS] = unsafe {std::mem::uninitialized()};
 
+  //Create an array of closures that generate a sine wave in gens[] 
+  //TODO: factor the closure itself out of this loop, allowing assignment to
+  //sin or tri or pipeline or whatever
   for i in 0..NUMGENS {
+    
     let mut clk = 0f32;
     let mut freq = c_scale[i];
     gens[i] = move || {
       clk = (clk + 1.0) % sample_rate;
       (clk * freq * 2.0 * 3.1415926 / sample_rate).sin()
     };
+
+    //Initialize my local amplitude array
     amps[i] = 0.5;
   } 
 
-  // Produce a sinusoid of maximum amplitude.
+  //get a sample from the generator closure stored at gens[gen]
   let mut getgen = |gen : usize| {
     gens[gen]()
   };
 
+  //Only occasionally update amps from vamps by counting down from finit and
+  //copying vamps in to amps at zero. next_value is the audio subsystem callback
+  //that gets called every 1/sample_rate seconds
   let finit = (sample_rate / 2000.) as u32; //every 2000 frames (~30x a second)
   let mut fcount = finit;
   let mut next_value = move || {
@@ -123,7 +133,8 @@ fn do_audio(vamps: [Arc<Mutex<f32>>; NUMGENS]) {
 
   };
 
-
+  //Unmodified event loop for sending samples to the audio hardware, taken from
+  //the cpal examples
   event_loop.run(move |_, data| {
       match data {
           cpal::StreamData::Output { buffer: cpal::UnknownTypeOutputBuffer::U16(mut buffer) } => {
